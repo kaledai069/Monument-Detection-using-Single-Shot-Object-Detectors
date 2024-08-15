f82e4037b5be0924737adfe3e3f7e781132617cc
kaledai069 <parzival.cooler@gmail.com>
2024-08-15 12:09:59 +0545

    pending updates

--- models/header.py
+++ models/header.py
@@ -3,23 +3,23 @@ from tensorflow.keras.layers import Layer, Input, Conv2D, MaxPool2D, Activation,
 from tensorflow.keras.regularizers import l2
 
 class HeadWrapper(Layer):
-    """Merging all feature maps for detections.
-    inputs:
-        conv4_3 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
-            ssd300 conv4_3 shape => (38 x 38 x 4) = 5776
-        conv7 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
-            ssd300 conv7 shape => (19 x 19 x 6) = 2166
-        conv8_2 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
-            ssd300 conv8_2 shape => (10 x 10 x 6) = 600
-        conv9_2 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
-            ssd300 conv9_2 shape => (5 x 5 x 6) = 150
-        conv10_2 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
-            ssd300 conv10_2 shape => (3 x 3 x 4) = 36
-        conv11_2 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
-            ssd300 conv11_2 shape => (1 x 1 x 4) = 4
-                                           Total = 8732 default box
-
-    outputs:
+    """
+    Merging all feature maps for detections.
+    Inputs:
+        first_conv layer = (batch_size, (layer_shape x aspect_ratios), last_dimension)
+            shape => (32 x 32 x 4) = 4096
+        second_conv layer = (batch_size, (layer_shape x aspect_ratios), last_dimension)
+            shape => (16 x 16 x 6) = 1536
+        extra1_2 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
+            shape => (8 x 8 x 6) = 384
+        extra2_2 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
+            shape => (4 x 4 x 6) = 96
+        extra3_2 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
+            shape => (2 x 2 x 4) = 16
+        extra4_2 = (batch_size, (layer_shape x aspect_ratios), last_dimension)
+            shape => (1 x 1 x 4) = 4
+                                           Total = 6132 default box
+    Outputs:
         merged_head = (batch_size, total_prior_boxes, last_dimension)
     """
 
@@ -38,33 +38,41 @@ class HeadWrapper(Layer):
         outputs = []
         for conv_layer in inputs:
             outputs.append(tf.reshape(conv_layer, (batch_size, -1, last_dimension)))
-        #
         return tf.concat(outputs, axis=1)
 
 def get_head_from_outputs(hyper_params, outputs):
-    """Generating ssd bbox delta and label heads.
-    inputs:
+    """
+    Generating SSDLite Bounding Box deltas and label heads.
+    
+    Inputs:
         hyper_params = dictionary
         outputs = list of ssd layers output to be used for prediction
 
-    outputs:
+    Outputs:
         pred_deltas = merged outputs for bbox delta head
         pred_labels = merged outputs for bbox label head
+        
     """
+    
+    ## Setting up regularization hyperparameters for Training the model ##
     label_reg_factor = 5e-3
     box_reg_factor = 1e-3
     total_labels = hyper_params["total_labels"]
+    
     # +1 for ratio 1
     len_aspect_ratios = [len(x) + 1 for x in hyper_params["aspect_ratios"]]
     labels_head = []
     boxes_head = []
+    
+    ## Stacking and Replacing Separable Depthwise Convolutional Block instead of Standard Convolution in Detection Head Layer ##
     for i, output in enumerate(outputs):
         aspect_ratio = len_aspect_ratios[i]
-        # labels_head.append(Conv2D(aspect_ratio * total_labels, (3, 3), padding="same", name="{}_conv_label_output".format(i+1))(output))
-        # boxes_head.append(Conv2D(aspect_ratio * 4, (3, 3), padding="same", name="{}_conv_boxes_output".format(i+1))(output))
+        
         labels_head.append(SeparableConv2D(filters = aspect_ratio * total_labels, kernel_size = (3, 3), padding = "same", name = f"{i+1}_separable_conv_label_output", depthwise_regularizer = l2(label_reg_factor), pointwise_regularizer = l2(label_reg_factor))(output))
+        
         boxes_head.append(SeparableConv2D(filters = aspect_ratio * 4, kernel_size = (3, 3), padding = "same", name = f"{i+1}_separable_conv_boxes_output", depthwise_regularizer = l2(box_reg_factor), pointwise_regularizer = l2(box_reg_factor))(output))
     
+    ## Custom Layer to wrap all the layers in Detection Head into a single layer ##
     pred_labels = HeadWrapper(total_labels, name="labels_head")(labels_head)
     pred_labels = Activation("softmax", name="conf")(pred_labels)
     
--- models/ssd_mobilenet_v2.py
+++ models/ssd_mobilenet_v2.py
@@ -6,40 +6,56 @@ from tensorflow.keras.regularizers import l2
 from .header import get_head_from_outputs
 
 def get_model(hyper_params):
-    """Generating ssd model for hyper params.
-    inputs:
+    """Creating MobileNetV2-SSDLite with Tensorflow Functional API for Model Building
+    
+    Inputs:
         hyper_params = dictionary
 
-    outputs:
+    Outputs:
         ssd_model = tf.keras.model
+        
     """
-    reg_factor = 1e-3
+    ## Setting Hyperparameters required by the Model ##
+    reg_factor = 4e-3
     img_size = hyper_params["img_size"]
-    base_model = MobileNetV2(include_top=False, input_shape=(img_size, img_size, 3))
+    
+    ## Loading pre-trained MobileNet-V2 model on Sinlge Monument Image Dataset ##
+    pre_trained_model = tf.keras.models.load_model("/Saved_Model/mobilenetv2_512x512_fine-tuned.h5")
+    base_model = pre_trained_model.layers[1]
+    
+    ## Input Layer ##
     input = base_model.input
+    
+    ## First Feature Map ##
     first_conv = base_model.get_layer("block_13_expand_relu").output
+    
+    ## Second Feature Map ##
     second_conv = base_model.output
-    #
-    ############################ Extra Feature Layers Start ############################
+    
+    ## Third Feature Map ##
     extra1_1 = Conv2D(256, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra1_1", kernel_regularizer = l2(reg_factor))(second_conv)
     extra1_2 = Conv2D(512, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra1_2", kernel_regularizer = l2(reg_factor))(extra1_1)
-    #
+    
+    ## Fourth Feature Map ##
     extra2_1 = Conv2D(128, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra2_1", kernel_regularizer = l2(reg_factor))(extra1_2)
     extra2_2 = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra2_2", kernel_regularizer = l2(reg_factor))(extra2_1)
-    #
+    
+    ## Fifth Feature Map ##
     extra3_1 = Conv2D(128, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra3_1", kernel_regularizer = l2(reg_factor))(extra2_2)
     extra3_2 = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra3_2", kernel_regularizer = l2(reg_factor))(extra3_1)
-    #
+    
+    ## Sixth Feature Map ##
     extra4_1 = Conv2D(128, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra4_1", kernel_regularizer = l2(reg_factor))(extra3_2)
     extra4_2 = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra4_2", kernel_regularizer = l2(reg_factor))(extra4_1)
-    ############################ Extra Feature Layers End ############################
+
+    ## Attaching 6-Feature Maps into a Single-Custom Detection Head Layer ##
     pred_deltas, pred_labels = get_head_from_outputs(hyper_params, [first_conv, second_conv, extra1_2, extra2_2, extra3_2, extra4_2])
     return Model(inputs=input, outputs=[pred_deltas, pred_labels])
 
 def init_model(model):
-    """Initializing model with dummy data for load weights with optimizer state and also graph construction.
+    """
+    Initializing model with dummy data for load weights with optimizer state and also graph construction.
     inputs:
         model = tf.keras.model
-
     """
-    model(tf.random.uniform((1, 300, 300, 3)))
+    model(tf.random.uniform((1, 512, 512, 3)))
